fdbd67ba9eeef4f0cbb849309cfdc674165ebd44
Ankit <ankitpandey2812@gmail.com>
2022-06-21 01:07:04 +0530

    misaligned changed

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 #include <assert.h>
-
+int getPairNumber(int majorColorIdx, int minorColorIdx)
+{       
+    return majorColorIdx*5+minorColorIdx;  
+}
+const char* getMajorColor(int majorColorIdx, int minorColorIdx, const char* majorColor[])
+{       
+        int pairNumber = getPairNumber(majorColorIdx, minorColorIdx);
+		int majorColorIndex = (pairNumber-1) / 5;
+		
+		return majorColor[majorColorIndex];
+}	
+const char* getMinorColor(int majorColorIdx, int minorColorIdx, const char* minorColor[])
+{
+	    int pairNumber = getPairNumber(majorColorIdx, minorColorIdx);
+		int minorColorIndex = (pairNumber) % 5;
+		
+		return minorColor[minorColorIndex];
+}
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            std::cout << i * 5 + j << " | " << getMajorColor(i,j, majorColor) << " | " << getMinorColor(i,j , minorColor) << "\n";
+            assert(getPairNumber(i,j) == (i*5+j)+1);
+            assert(getMajorColor(i,j,majorColor) == majorColor[i]);
+            assert(getMinorColor(i,j,minorColor) == minorColor[j]);
+            
         }
     }
     return i * j;
