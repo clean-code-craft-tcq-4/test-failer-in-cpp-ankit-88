7d23cd1f8353e6c6f90211edb1e41ed4268fea3c
Ankit <ankitpandey2812@gmail.com>
2022-06-20 23:45:37 +0530

    tshirt and alerter first check

--- alerter.cpp
+++ alerter.cpp
@@ -21,6 +21,9 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
+	else {
+		alertFailureCount+=1;
+	}	
 }
 
 int main() {
